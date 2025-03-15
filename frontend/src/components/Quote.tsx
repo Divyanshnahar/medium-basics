2eb8b266f8e3699450ecfcdcf7084263d6431b0b
Divyansh Nahar <divyanshnahar15@gmail.com>
2025-03-16 00:08:39 +0530

    frontend initialized

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,14 @@
+export const Quote = () => {
+    return (
+      <div className="p-8 md:p-16">
+        <blockquote className="text-2xl font-semibold italic text-gray-900">
+          “The customer service I received was exceptional. The support team went above and beyond to address my concerns.”
+        </blockquote>
+        <footer className="mt-4">
+          <p className="text-lg font-bold">Divyansh Nahar</p>
+          <p className="text-gray-500">CEO, BLASTprod Inc</p>
+        </footer>
+      </div>
+    );
+  };
+  
